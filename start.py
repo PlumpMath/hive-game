0185150217fa58d24035713c13c4b8defc1ac048
Elijah Malaby <qwe12345678910@gmail.com>
2015-02-13 10:03:19 -0500

    Created basic start.py file.

--- start.py
+++ start.py
@@ -1,1 +1,12 @@
 # Code to bring engine and graphics subsystems goes here.
+
+from . import graphics, engine
+
+engine = engine.Engine() # subclass of Thread?
+
+graphics = graphics.GUI(engine)
+
+engine.start()
+
+graphics.show()
+
