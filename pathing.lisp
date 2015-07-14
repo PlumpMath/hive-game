9fa26229706f89a4d8927b8d9c12de97b876e865
Elijah Malaby <qwe12345678910@gmail.com>
2015-07-14 14:24:22 -0400

    Abstracted away coordinate handling and pathing array access.

--- pathing.lisp
+++ pathing.lisp
@@ -45,7 +45,17 @@
                                        (setf update t
                                              (aref updates i j) 0)
                                        (update-neighbors i j)))))))))
-(defparameter *map-size* '(8 8))
+(defun make-coord (x y)
+  (cons x y))
+(defun get-x-y (coord)
+  (values (car coord) (cdr coord)))
+(defun lookup-coord (map coord)
+  (multiple-value-bind (x y) (get-x-y coord)
+    (aref map x y)))
+(defun (setf lookup-coord) (value map coord)
+  (multiple-value-bind (x y) (get-x-y coord)
+    (setf (aref map x y) value)))
+(defparameter *map-size* (make-coord 8 8))
 
 (defclass path ()
   ((obstacles :initform (make-array *map-size* :element-type '(mod 2))
@@ -63,24 +73,24 @@
    (global-updates :initform (cons :glob nil)
                    :type 'cons)))
 
-(defun new-obstacle-p (path coords)
-  (with-slots (obstacles map) path
-    (destructuring-bind (x y) coords
-      (and (= 1 (aref obstacles x y))
-           (< 0 (aref map x y))))))
-(defun new-goal-p (path coords)
-  (with-slots (goals map) path
-    (destructuring-bind (x y) coords
-      (and (= 1 (aref goals x y))
-           (> 255 (aref map x y))))))
 
-(defun get-neighbors (coords)
-  (destructuring-bind (x y) coords
-    (destructuring-bind (i j) *map-size*
-      `((,(mod (+ x 1) i) ,y)
-        (,(mod (- x 1) i) ,y)
-        (,x ,(mod (+ y 1) j))
-        (,x ,(mod (- y 1) j))))))
+
+(defun new-obstacle-p (path coord)
+  (with-slots (obstacles map) path
+    (and (= 1 (lookup-coord obstacles coord))
+         (< 0 (lookup-coord obstacles coord)))))
+(defun new-goal-p (path coord)
+  (with-slots (goals map) path
+    (and (= 1 (lookup-coord goals coord))
+         (> 255 (lookup-coord map coord)))))
+
+(defun get-neighbors (coord)
+  (multiple-value-bind (x y) (get-x-y coord)
+    (multiple-value-bind (i j) (get-x-y *map-size*)
+      `(,(make-coord (mod (+ x 1) i) y)
+        ,(make-coord (mod (- x 1) i) y)
+        ,(make-coord x (mod (+ y 1) j))
+        ,(make-coord x (mod (- y 1) j))))))
 
 (defun path-map%% (path)
   (declare (type path path))
@@ -117,55 +127,61 @@
   (destructuring-bind (x y) (array-dimensions array)
     (loop :for i :below x :do
       (loop :for j :below y :do
-        (format t "~3a " (aref array i j)))
+        (format t "~3a " (lookup-coord array (make-coord i j))))
       (format t "~%"))))
 
 (defun update-coord (path con coord)
   (with-slots (obstacles goals active map) path
-    (destructuring-bind (x y) coord
-      (cond
-        ((new-obstacle-p path coord)
-         (setf (aref map x y) 0)
-         (nconc con (get-neighbors coord)))
-        ((new-goal-p path coord)
-         (setf (aref map x y) 255)
-         (nconc con (get-neighbors coord)))
-        (t
-         (let ((new (- (apply #'max (mapcar (lambda (coord) (aref map
-                                                                   (car coord)
-                                                                   (cadr coord)))
-                                            (get-neighbors coord))) 1)))
-           (unless (or (= new (aref map x y))
-                       (= 1 (aref obstacles x y))
-                       (= 1 (aref goals x y))
-                       (> 0 new))
-             (print new)
-             (setf (aref map x y) new)
-             (nconc con (get-neighbors coord)))))))))
+    (cond
+      ((new-obstacle-p path coord)
+       (setf (lookup-coord map coord) 0)
+       (nconc con (get-neighbors coord)))
+      ((new-goal-p path coord)
+       (setf (lookup-coord map coord) 255)
+       (nconc con (get-neighbors coord)))
+      (t
+       (let ((new (- (apply #'max (mapcar (lambda (coord) (lookup-coord map coord))
+                                          (get-neighbors coord)))
+                     1)))
+         (unless (or (= new (lookup-coord map coord))
+                     (= 1 (lookup-coord obstacles coord))
+                     (= 1 (lookup-coord goals coord))
+                     (> 0 new))
+           (setf (lookup-coord map coord) new)
+           (nconc con (get-neighbors coord))))))))
 
 (defun path-map%%% (path)
   (with-slots (active map local-updates global-updates) path
     (if (cdr global-updates)
         (loop :for con = (cdr global-updates) :then (cdr con)
-              :do (update-coord path local-updates (car con))
-                  (setf (aref active (caar con) (cadar con)) 1)
+              :if (= 1 (lookup-coord active (car con))) :do
+                (update-coord path local-updates (car con))
               :while (cdr con)
               :finally (setf global-updates con)))
     (if (cdr local-updates)
         (loop :for con = (cdr local-updates) :then (cdr con)
               :do (update-coord path con (car con))
-                  (setf (aref active (caar con) (cadar con)) 1)
+                  (setf (lookup-coord active (car con)) 1)
               :while (cdr con)
               :finally (setf local-updates con)))))
 
 (defun add-obstacle (path coord)
   (with-slots (obstacles local-updates) path
-    (destructuring-bind (x y) coord
-      (setf (aref obstacles x y) 1)
-      (nconc local-updates (list coord)))))
+    (setf (lookup-coord obstacles coord) 1)
+    (nconc local-updates (list coord))))
 (defun add-goal (path coord)
   (with-slots (goals local-updates) path
-    (destructuring-bind (x y) coord
-      (setf (aref goals x y) 1)
-      (nconc local-updates (list coord)))))
+    (setf (lookup-coord goals map) 1)
+    (nconc local-updates (list coord))))
 
+
+(defun make-goals-by-targets (target-map)
+  "Create map of outlines. Used to find squares adjacent to a given building, for example."
+  (destructuring-bind (i j) (array-dimensions target-map)
+    (let ((goals (make-array '(i j) :element-type 'bit)))
+      (loop :for x :below i :do
+        (loop :for y :below j
+              :if (= 1 (aref target-map x y)) :do
+                (loop :for (x1 y1) :in (get-neighbors '(x y))
+                      :if (= 0 (aref target-map x1 y1)) :do
+                        (setf (aref goals x1 y1) 1)))))))
